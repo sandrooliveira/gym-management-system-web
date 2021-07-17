6a069b989fad02228ecfe3f6cfdbb29d11de936c
Sandro Augusto de Oliveira <sandroaugusto67@gmail.com>
2021-07-17 14:13:16 -0300

    Add more sample data to People page

--- src/pages/people/People.tsx
+++ src/pages/people/People.tsx
@@ -36,6 +36,36 @@ export const People = () => {
       picture: require('../../assets/images/face2.jpg').default,
       theme: "expired",
       footerText: "Período de acesso vencido em 20/05/2021",
+    },
+    {
+      name: "Maria Tereza Reis",
+      picture: require('../../assets/images/face3.jpg').default,
+      theme: "expired",
+      footerText: "Período de acesso vencido em 20/05/2021",
+    },
+    {
+      name: "Cíntia do Carmo Lopes",
+      picture: require('../../assets/images/aliciana.jpg').default,
+      theme: "blocked",
+      footerText: "Acesso Bloqueado"
+    },
+    {
+      name: "Anderson Lopes Reis",
+      picture: require('../../assets/images/face1.png').default,
+      theme: "active",
+      footerText: "Acesso Liberado até 20/07/2021"
+    },
+    {
+      name: "Josefa Lucia Faria",
+      picture: require('../../assets/images/face2.jpg').default,
+      theme: "expired",
+      footerText: "Período de acesso vencido em 20/05/2021",
+    },
+    {
+      name: "Cida Tereza Reis",
+      picture: require('../../assets/images/face3.jpg').default,
+      theme: "expired",
+      footerText: "Período de acesso vencido em 20/05/2021",
     }
   ]
 
